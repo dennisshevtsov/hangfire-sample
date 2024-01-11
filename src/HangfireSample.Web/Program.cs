79b53a0905d122b42ed9435192b428e13768a5ea
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-11 11:37:07 -0800

    add hangfire

--- src/HangfireSample.Web/Program.cs
+++ src/HangfireSample.Web/Program.cs
@@ -1,4 +1,10 @@
+using Hangfire;
+using Hangfire.PostgreSql;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHangfire(configuration => configuration.UsePostgreSqlStorage(options => options.UseNpgsqlConnection("")));
+
 WebApplication app = builder.Build();
+app.UseHangfireDashboard();
 app.MapGet("/", () => "Hangfire sample working...");
 app.Run();
