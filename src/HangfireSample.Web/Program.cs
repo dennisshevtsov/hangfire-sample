e7b3b8f9adbc0e422facf003afd5388f1643a47a
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-12 02:55:36 -0800

    add connection string

--- src/HangfireSample.Web/Program.cs
+++ src/HangfireSample.Web/Program.cs
@@ -2,7 +2,8 @@ using Hangfire;
 using Hangfire.PostgreSql;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHangfire(configuration => configuration.UsePostgreSqlStorage(options => options.UseNpgsqlConnection("")));
+builder.Services.AddHangfire(configuration =>
+  configuration.UsePostgreSqlStorage(options => options.UseNpgsqlConnection(builder.Configuration.GetConnectionString("HANGFIRE"))));
 
 WebApplication app = builder.Build();
 app.UseHangfireDashboard();
