4129ec4dbba13b3619b852c78d9e140c8986d188
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-13 09:25:32 -0800

    allow anonymous access

---
+++ src/HangfireSample.Web/AnonymousAuthorizationFilter.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace HangfireSample.Web;
+
+public sealed class AnonymousAuthorizationFilter : IDashboardAuthorizationFilter
+{
+  public bool Authorize([NotNull] DashboardContext context) => true;
+}
